38032bb42759e5e42f686a017a118d8dd1b0cfee
louis mane <dennis.itua1@gmail.com>
2021-04-11 17:38:37 -0400

    updated Header and Footer components

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,7 +4,7 @@ import '../Calculator/index.css';
 function Footer() {
     return (
          <div>
-            <footer class="footer sticky-bottom footer-light bg-light">
+            <footer className="footer sticky-bottom footer-light bg-light">
                 <p><small>View calculator GitHub repository <a id="contact-me-footer" href="https://github.com/Dennis-The14th-web/calculator">here</a>. This is a React Js application <b>&#9400; 2020 Dennis Itua </b></small></p>
             </footer>
         </div>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,8 +5,8 @@ import '../Header/index.css';
 function Header() {
     return (
         <div>
-            <nav class="navbar sticky-top navbar-light bg-light">
-                <h2 class="navbar-brand">Basic Calculator</h2>
+            <nav className="navbar sticky-top navbar-light bg-light">
+                <h2 className="navbar-brand">Basic Calculator</h2>
             </nav>
         </div>
     )
