0f76bb12e7ce99073c72b4739c0f2d70ff1950b7
louis mane <dennis.itua1@gmail.com>
2021-04-11 18:03:19 -0400

    updated footer sticky footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import '../Calculator/index.css';
+import '../Footer/index.css';
 
 function Footer() {
     return (
          <div>
-            <footer className="footer sticky-bottom footer-light bg-light">
-                <p><small>View calculator GitHub repository <a id="contact-me-footer" href="https://github.com/Dennis-The14th-web/calculator">here</a>. This is a React Js application <b>&#9400; 2020 Dennis Itua </b></small></p>
+            <footer className="footer mt-auto py-3 bg-light">
+                <div className="container">
+                <span className="text-muted"><small>View calculator GitHub repository <a id="contact-me-footer" href="https://github.com/Dennis-The14th-web/calculator">here</a>. This is a React Js application <b>&#9400; 2020 Dennis Itua </b></small></span>
+                </div>
             </footer>
         </div>
     )
