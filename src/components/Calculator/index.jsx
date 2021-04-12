7b4b1ba2f6c313342cb2ba5606fb82fb9e03e53a
louis mane <dennis.itua1@gmail.com>
2021-04-12 01:03:26 -0400

    updated styles for new operators

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -10,31 +10,33 @@ function Calculator (props){
             <div className="result">{props.result}</div>
             </div>
             <div className="row">
+            <button className="btn btn-light btnNum style" onClick={props.clearResult} name="Clear">AC</button>
+            <button className="btn btn-light btnNum style" onClick={props.clearResult} name="Clear">C</button>
+            <button className="btn btn-light btnNum style" onClick={props.clearResult} name="Clear">%</button>
+            <button className="btn btn-light btnNum operator" onClick={props.div} name="/">÷</button>
+            </div>
+            <div className="row">
             <button className="btn btn-light btnNum" onClick={e=>props.handleBtn(e.target.name)} name="7">7</button>
             <button className="btn btn-light btnNum" onClick={e=>props.handleBtn(e.target.name)} name="8">8</button>
             <button className="btn btn-light btnNum" onClick={e=>props.handleBtn(e.target.name)} name="9">9</button>
-            <button className="btn btn-light btnNum operator" onClick={props.div} name="/">÷</button>
+            <button className="btn btn-light btnNum operator" onClick={props.multi} name="x">x</button>
             </div>
             <div className="row">
             <button className="btn btn-light btnNum" onClick={e=>props.handleBtn(e.target.name)} name="4">4</button>
             <button className="btn btn-light btnNum" onClick={e=>props.handleBtn(e.target.name)} name="5">5</button>
             <button className="btn btn-light btnNum" onClick={e=>props.handleBtn(e.target.name)} name="6">6</button>
-            <button className="btn btn-light btnNum operator" onClick={props.multi} name="x">x</button>
+            <button className="btn btn-light btnNum operator" onClick={props.sum} name="+">+</button>
             </div>
             <div className="row">
             <button className="btn btn-light btnNum" onClick={e=>props.handleBtn(e.target.name)} name="1">1</button>
             <button className="btn btn-light btnNum" onClick={e=>props.handleBtn(e.target.name)} name="2">2</button>
             <button className="btn btn-light btnNum" onClick={e=>props.handleBtn(e.target.name)} name="3">3</button>
-            <button className="btn btn-light btnNum operator" onClick={props.sum} name="+">+</button>
-            </div>
-            <div className="row">
-            <button className="btn btn-light btnNum" onClick={props.addZero} name="0">0</button>
-            <button className="btn btn-light btnNum" onClick={props.addDecimal} name=".">.</button>
-            <button className="btn btn-light btnNum" onClick={e=>props.eval(e)} name="=">=</button>
             <button className="btn btn-light btnNum operator" onClick={props.sub} name="-">-</button>
             </div>
             <div className="row">
-            <button className="btn btn-light btnNum" onClick={props.clearResult} name="Clear" id="clear-btn">Clear</button>
+            <button className="btn btn-light btnNum" id="clear-btn" onClick={props.addZero} name="0">0</button>
+            <button className="btn btn-light btnNum" onClick={props.addDecimal} name=".">.</button>
+            <button className="btn btn-light btnNum" onClick={e=>props.eval(e)} name="=">=</button>
             </div>
             </div>
         </div>
